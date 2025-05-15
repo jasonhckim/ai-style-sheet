04b4e965d2d8343620bfc48a940a4597e6d43731
Jason Kim <95268946+jasonhckim@users.noreply.github.com>
2025-05-15 13:03:04 -0700

    Update drive_utils.py

--- gcp/shared/drive_utils.py
+++ gcp/shared/drive_utils.py
@@ -1,1 +1,9 @@
+from shared.auth import get_drive_service
 
+def copy_template_sheet(new_title, destination_folder_id, template_id, creds):
+    drive = get_drive_service()
+    copied_file = drive.files().copy(
+        fileId=template_id,
+        body={"name": new_title, "parents": [destination_folder_id]}
+    ).execute()
+    return copied_file["id"]
