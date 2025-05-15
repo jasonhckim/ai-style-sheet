bd50debcc6bac53a57c6c25a2634ab96f77329ce
Jason Kim <95268946+jasonhckim@users.noreply.github.com>
2025-05-15 13:19:11 -0700

    Update common.py

--- gcp/shared/common.py
+++ gcp/shared/common.py
@@ -1,1 +1,6 @@
+from google.cloud import secretmanager
 
+def access_secret(secret_name: str, project_id: str):
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
+    return client.access_secret_version(request={"name": name}).payload.data.decode("UTF-8")
